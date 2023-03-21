4d125969e2b1aeed554cf4a04f3f10bf08017644
alicebook12220 <alicebook12220@gmail.com>
2023-03-21 16:11:50 +0800

    Update train.py

--- yolov8/train.py
+++ yolov8/train.py
@@ -4,11 +4,11 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolov8m.yaml")  # build a new model from scratch
-    model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolov8l.yaml")  # build a new model from scratch
+    model = YOLO("yolov8l.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="scratch_v3.yaml", epochs=500, batch=32, workers=4)  # train the model
+    model.train(data="coco128.yaml", epochs=500, batch=32, workers=4)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
-    results = model("D:/harrylin/smart_image/datasets/Scratch/AAMORA00.B160UAN03_ET.B16UAN3_P4_ASI1229.5Q2A86N600.5Q2A86N63.202.b.1662857460.jpg")  # predict on an image
-    success = model.export(format="onnx")  # export the model to ONNX format
+    #results = model("D:/harrylin/smart_image/datasets/Scratch/AAMORA00.B160UAN03_ET.B16UAN3_P4_ASI1229.5Q2A86N600.5Q2A86N63.202.b.1662857460.jpg")  # predict on an image
+    #success = model.export(format="onnx")  # export the model to ONNX format
